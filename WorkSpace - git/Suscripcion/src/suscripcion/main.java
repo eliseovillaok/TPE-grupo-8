4d50a24ce998dc1e6d26a886a869b9660d3d5ff2
Eliseo <eliseovilla10@gmail.com>
2023-06-04 19:33:53 -0300

    Main final

--- WorkSpace - git/Suscripcion/src/suscripcion/main.java
+++ WorkSpace - git/Suscripcion/src/suscripcion/main.java
@@ -3,13 +3,22 @@ package suscripcion;
 public class main {
 	
 	public static void main(String[] args) {
-		// main hecho a ojo
 		Cliente c1 = new Cliente("Pedro", "Picapiedras", "44298732", "pedrito2008", "pedropica@gmail.com");
-		Suscripcion s1 = new Suscripcion("Usuhaia", "Mar de las pampas");
+		Plataforma p1 = new Plataforma();
 		
+		// Creamos nueva suscripcion
+		p1.addSuscripcion("Usuhaia", "Mar de las pampas");
+		// Sacamos pasaje
 		c1.sacarPasaje("Usuhaia", "Mar de las pampas");
-		c1.suscribirse(s1);
+		
+		// Obtenemos la suscripcion
+		Suscripcion s1 = p1.getSuscripcion("Usuhaia", "Mar de las pampas");
+		
+		// Agregamos un suscriptor y notificamos
+		s1.addSuscriptor(c1.getUsuario());
 		s1.notificar();
+		
+		
 	}
 
 }
